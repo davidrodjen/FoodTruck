2d7b0e9c94c602336d54668ea90ab5683806094e
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-04-14 09:18:55 -0700

    Added Data Annotations

--- FoodTruck/Models/Account.cs
+++ FoodTruck/Models/Account.cs
@@ -8,18 +8,28 @@ namespace FoodTruck.Models
 {
     public class Account
     {
+        [Key]
         public int AccountId { get; set; }
 
+        [Required]
+        [StringLength(15)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(20)]
         public string LastName { get; set; }
 
+        [Required]
         public string Address { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
         public int PhoneNumber { get; set; }
 
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
