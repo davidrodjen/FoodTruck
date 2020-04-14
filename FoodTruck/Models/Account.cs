7bb0442c5587db391af82475c3f229a69228097c
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-04-14 09:17:01 -0700

    Added password property

--- FoodTruck/Models/Account.cs
+++ FoodTruck/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,7 @@ namespace FoodTruck.Models
         public string Email { get; set; }
 
         public int PhoneNumber { get; set; }
+
+        public string Password { get; set; }
     }
 }
