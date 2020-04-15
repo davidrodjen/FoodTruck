9cab21a5519c75fcae80ae9f59af73a368a0b176
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-15 15:03:09 -0700

    Set initial required datatypes

--- FoodTruck/Models/FoodItem.cs
+++ FoodTruck/Models/FoodItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,17 +9,23 @@ namespace FoodTruck.Models
     public class FoodItem
     {
 
-
+        [Key]
+        [Required]
         public int ItemID { get; set; }
 
+        [Required]
         public string ItemName { get; set; }
 
+        [Required]
         public double Price { get; set; }
 
+        [Required]
         public string Descrip { get; set; }
 
+        [Required]
         public int Calories { get; set; }
 
+        [Required]
         public bool Vegan { get; set; }
 
 
