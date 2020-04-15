4111ef3de6debd9a779574f2d0c757388fb1a72f
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-15 15:01:41 -0700

    Created initial properties

--- FoodTruck/Models/FoodItem.cs
+++ FoodTruck/Models/FoodItem.cs
@@ -7,5 +7,20 @@ namespace FoodTruck.Models
 {
     public class FoodItem
     {
+
+
+        public int ItemID { get; set; }
+
+        public string ItemName { get; set; }
+
+        public double Price { get; set; }
+
+        public string Descrip { get; set; }
+
+        public int Calories { get; set; }
+
+        public bool Vegan { get; set; }
+
+
     }
 }
