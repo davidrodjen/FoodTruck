c689faf8554393b54666294ec13968010dc417d2
David Rodriguez-Jenkins <53097291+davidrodjen@users.noreply.github.com>
2020-04-28 10:07:20 -0700

    Revert "Issue#25 configure input model (#30)"
    This reverts commit b11c6a98ee02f2bca3f1f83af219bc538f3ada2f.

---
+++ FoodTruck/Models/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTruck.Models
+{
+    /// <summary>
+    /// A single user account
+    /// </summary>
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+
+        /// <summary>
+        /// The User's First Name
+        /// </summary>
+        [Required]
+        [StringLength(15)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The User's Last Name
+        /// </summary>
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The User's Delivery Address
+        /// </summary>
+        [Required]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// The User's Email
+        /// </summary>
+        [Required]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The User's Phone Number
+        /// </summary>
+        public int PhoneNumber { get; set; }
+
+
+        /// <summary>
+        /// The User's Password
+        /// </summary>
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
