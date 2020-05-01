16622abbe75315dc262a6073fd8f6cf6d1135abb
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-05-01 10:59:04 -0700

    Added properties

---
+++ FoodTruck/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTruck.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+
+        public string EventName { get; set; }
+
+        public int NumGuests { get; set; }
+
+        public string PackageDescription { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string Address { get; set; }
+
+        public double Price { get; set; }
+    }
+}
