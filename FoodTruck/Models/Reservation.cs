2f7fa9553deff8bdde3db53158c7454ef9cfd200
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-05-01 11:03:56 -0700

    Added Data Annotations

--- FoodTruck/Models/Reservation.cs
+++ FoodTruck/Models/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,18 +8,25 @@ namespace FoodTruck.Models
 {
     public class Reservation
     {
+        [Key]
         public int ReservationId { get; set; }
 
         public string EventName { get; set; }
 
+        [Required]
         public int NumGuests { get; set; }
 
+        [Required]
         public string PackageDescription { get; set; }
 
+        [Required]
         public DateTime DeliveryDate { get; set; }
 
-        public string Address { get; set; }
+        [Required]
+        public string Location { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
     }
 }
