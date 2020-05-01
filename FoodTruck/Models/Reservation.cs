83d50e9a8266ddc70df5131ec31da99bbc64aa40
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-05-01 11:11:22 -0700

    Added documentation

--- FoodTruck/Models/Reservation.cs
+++ FoodTruck/Models/Reservation.cs
@@ -8,23 +8,44 @@ namespace FoodTruck.Models
 {
     public class Reservation
     {
+        /// <summary>
+        /// Unique Identifier for a single reservation
+        /// </summary>
         [Key]
         public int ReservationId { get; set; }
 
+        /// <summary>
+        /// The name of the event. Ex: Maggie's Birthday
+        /// </summary>
         public string EventName { get; set; }
 
+        /// <summary>
+        /// The number of guests that will be fed
+        /// </summary>
         [Required]
         public int NumGuests { get; set; }
 
+        /// <summary>
+        /// The description of the food package
+        /// </summary>
         [Required]
         public string PackageDescription { get; set; }
 
+        /// <summary>
+        /// The date and time of the event 
+        /// </summary>
         [Required]
         public DateTime DeliveryDate { get; set; }
 
+        /// <summary>
+        /// The address of the event
+        /// </summary>
         [Required]
         public string Location { get; set; }
 
+        /// <summary>
+        /// The price for the reservation. Calculated per person
+        /// </summary>
         [Required]
         [DataType(DataType.Currency)]
         public double Price { get; set; }
