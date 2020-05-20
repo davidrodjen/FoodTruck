2f07dd98a337fd6dde16d2f3441e823eac85a36e
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-20 10:10:09 -0700

    Created initial reservation controller

---
+++ FoodTruck/Controllers/ReservationController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodTruck.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ApplicationDbContext _context; // Only constructor can edit
+
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reservation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reservation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservation/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
