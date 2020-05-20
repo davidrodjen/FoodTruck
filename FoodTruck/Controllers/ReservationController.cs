7d9ae14ba45c61baa92fe7572230018e9fe2ae58
David Rodriguez-Jenkins <53097291+davidrodjen@users.noreply.github.com>
2020-05-20 10:19:04 -0700

    Issue#74 create reservation controller (#75)
    * Created initial reservation controller
    
    * Cut down on the code to just have the Add which will be implemented later.

---
+++ FoodTruck/Controllers/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodTruck.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ApplicationDbContext _context; // Only constructor can edit
+
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
