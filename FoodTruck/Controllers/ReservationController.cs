6c0363d82c2dced9c19295e390326ce1b87b3d06
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-20 10:17:26 -0700

    Cut down on the code to just have the Add which will be implemented later.

--- FoodTruck/Controllers/ReservationController.cs
+++ FoodTruck/Controllers/ReservationController.cs
@@ -17,85 +17,12 @@ namespace FoodTruck.Controllers
             _context = context;
         }
 
-        // GET: Reservation
-        public ActionResult Index()
+
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
         }
 
-        // GET: Reservation/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Reservation/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Reservation/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reservation/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Reservation/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reservation/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Reservation/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
