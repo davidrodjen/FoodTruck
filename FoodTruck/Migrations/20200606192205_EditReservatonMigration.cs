45aef9d69c9db1f63c2a26a6c9ecfc4d1bd0438d
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-06-06 12:29:12 -0700

    Added Migrations for Edit connection to DB

---
+++ FoodTruck/Migrations/20200606192205_EditReservatonMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodTruck.Migrations
+{
+    public partial class EditReservatonMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
