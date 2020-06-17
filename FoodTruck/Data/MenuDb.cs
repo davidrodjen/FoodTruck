d316c142f445225bc626f9f5fa32aa5d79c6fe49
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-06-17 11:20:28 -0700

    Added Documentation

--- FoodTruck/Data/MenuDb.cs
+++ FoodTruck/Data/MenuDb.cs
@@ -9,7 +9,11 @@ namespace FoodTruck.Data
 {
     public class MenuDb
     {
-
+        /// <summary>
+        /// Retrieve all Food Items
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<List<FoodItem>> GetFoodItems(ApplicationDbContext context)
         {
             List<FoodItem> f = await (from FoodItem in context.FoodItem
@@ -17,6 +21,11 @@ namespace FoodTruck.Data
             return f;
 
         }
+        /// <summary>
+        /// Retrieve all reservations
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<List<Reservation>> GetReservations(ApplicationDbContext context)
         {
             List<Reservation> res = await (from Reservation in context.Reservations
@@ -24,6 +33,12 @@ namespace FoodTruck.Data
             return res;
         }
 
+        /// <summary>
+        /// Add a Food Item
+        /// </summary>
+        /// <param name="food"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<FoodItem> Add(FoodItem food, ApplicationDbContext context)
         {
             await context.AddAsync(food);
@@ -32,6 +47,26 @@ namespace FoodTruck.Data
             return food;
         }
 
+        /// <summary>
+        /// Edit a Food Item
+        /// </summary>
+        /// <param name="food"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static async Task<FoodItem> Edit(FoodItem food, ApplicationDbContext context)
+        {
+            await context.AddAsync(food);
+            context.Entry(food).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return food;
+        }
+
+        /// <summary>
+        /// Delete a Food Item
+        /// </summary>
+        /// <param name="food"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public async static Task Delete(FoodItem food, ApplicationDbContext context)
         {
             await context.AddAsync(food);
@@ -39,6 +74,12 @@ namespace FoodTruck.Data
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Get a Food Item by its ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<FoodItem> GetFoodItemById(int id, ApplicationDbContext context)
         {
             FoodItem food = await (from FoodItem in context.FoodItem
@@ -48,6 +89,12 @@ namespace FoodTruck.Data
             return food;
         }
 
+        /// <summary>
+        /// Get a Reservation by its ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<Reservation> GetReservationById(int id, ApplicationDbContext context)
         {
             Reservation reservation = await (from Reservation in context.Reservations
@@ -57,15 +104,13 @@ namespace FoodTruck.Data
             return reservation;
         }
 
-        public static async Task<FoodItem> Edit(FoodItem food, ApplicationDbContext context)
-        {
-            await context.AddAsync(food);
-            context.Entry(food).State = EntityState.Modified;
-            await context.SaveChangesAsync();
-            return food;
-        }
 
-
+        /// <summary>
+        /// Add a new Reservation
+        /// </summary>
+        /// <param name="res"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<Reservation> Add(Reservation res, ApplicationDbContext context)
         {
             await context.AddAsync(res);
@@ -73,13 +118,12 @@ namespace FoodTruck.Data
             return res;
         }
 
-        public async static Task DeleteReservation(Reservation res, ApplicationDbContext context)
-        {
-            await context.AddAsync(res);
-            context.Entry(res).State = EntityState.Deleted;
-            await context.SaveChangesAsync();
-        }
-
+        /// <summary>
+        /// Edit a Reservation
+        /// </summary>
+        /// <param name="res"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static async Task<Reservation> EditReservation(Reservation res, ApplicationDbContext context)
         {
             await context.AddAsync(res);
@@ -87,5 +131,19 @@ namespace FoodTruck.Data
             await context.SaveChangesAsync();
             return res;
         }
+
+        /// <summary>
+        /// Delete a Reservation
+        /// </summary>
+        /// <param name="res"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async static Task DeleteReservation(Reservation res, ApplicationDbContext context)
+        {
+            await context.AddAsync(res);
+            context.Entry(res).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+        }
+
     }
 }
